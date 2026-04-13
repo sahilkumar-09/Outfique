579c0d211a3f7466d9447047c98a9ae0e69c7577
sahilkumar-09 <9142sahilkumar@gmail.com>
2026-04-13 14:51:25 +0530

    initialize application routing with App component and basic home route

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { RouterProvider } from 'react-router'
+import { router } from './app.routes'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   )
 }
 
